a66bcf6b970a8e396c6d64a6a8cfdbba6d329c72
FulljsDeveloper <fulljs.developer@gmail.com>
2024-12-20 06:13:58 +0100

    u

--- init-replica.js
+++ init-replica.js
@@ -2,8 +2,6 @@
 
 rs.initiate({
     _id: "rs0",
-    members: [
-      { _id: 0, host: "db:27017" }
-    ]
+    members: [{ _id: 0, host: "db:27017" }]  // "db" doit être le nom du service MongoDB dans votre fichier Docker Compose
   });
   
